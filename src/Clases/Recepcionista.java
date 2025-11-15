5465fed0d8b553e4def6ea9a4dd5270d20f80243
Nahuel <paradacolo0@gmail.com>
2025-11-15 20:48:23 -0300

    Limpieza Recepcionista2

--- src/Clases/Recepcionista.java
+++ src/Clases/Recepcionista.java
@@ -1,42 +1,19 @@
 package Clases;
 
-import Enums.MetodoPago;
 import Enums.Turno;
-import Enums.estadoHabitacion;
-import Enums.tipoHabitacion;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Date;
 
 public class Recepcionista extends Usuario {
+    /// Atributos
     public static int contador = 0;
-
     private int id;
     private Turno turno;
+
+    /// Constructor
 
     public Recepcionista(Turno turno,String nombre, String contraseña) {
         super(nombre,contraseña);
         this.id = contador++;
         this.turno = turno;
     }
-
-    public boolean realizarCheckIn(MetodoPago pago, Date fechaInicio, Date fechaFinal, String dniP, String nombreP, String apellidoP, String gmailP, String telefonoP, String origenP, String domicilioP, int numeroH, tipoHabitacion tipoH, estadoHabitacion estadoH)
-    {
-        Pasajero pasajeroAux = new Pasajero(dniP, nombreP, apellidoP, gmailP, telefonoP, origenP, domicilioP);
-        Habitacion habitacionAux = new Habitacion(numeroH, tipoH, estadoH);
-        return new Reserva (pago, fechaInicio, fechaFinal, pasajeroAux, habitacionAux);
-    }
-
-    public void realizarCheckOut(MetodoPago pago, Date fechaInicio, Date fechaFinal, String dniP, String nombreP, String apellidoP, String gmailP, String telefonoP, String origenP, String domicilioP, int numeroH, tipoHabitacion tipoH, estadoHabitacion estadoH)
-    {
-        Pasajero pasajeroAux = new Pasajero(dniP, nombreP, apellidoP, gmailP, telefonoP, origenP, domicilioP);
-        Habitacion habitacionAux = new Habitacion(numeroH, tipoH, estadoH);
-        return new Reserva (pago, fechaInicio, fechaFinal, pasajeroAux, habitacionAux);
-    }
-
-
-
-
 
 }
