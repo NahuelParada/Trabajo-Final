eaba549e33de71af9c588d054e3f74f060e77ce3
Nahuel <paradacolo0@gmail.com>
2025-11-17 12:25:07 -0300

    Metodos y atributo precio por Noche en Habitacion

--- src/Clases/Habitacion.java
+++ src/Clases/Habitacion.java
@@ -9,23 +9,56 @@ public class Habitacion implements Identificador {
     private int numHabitacion;
     private TipoHabitacion tipo;
     private EstadoHabitacion estado;
+    private double precioXNoche;
     /// Constructor
-    public Habitacion(int numHabitacion, TipoHabitacion tipo, EstadoHabitacion estado) {
+    public Habitacion(int numHabitacion, TipoHabitacion tipo, EstadoHabitacion estado, double precioXNoche) {
         this.numHabitacion = numHabitacion;
         this.tipo = tipo;
         this.estado = estado;
+        this.precioXNoche = precioXNoche;
     }
+    /// Metodos
+
+    public EstadoHabitacion getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoHabitacion estado) {
+        this.estado = estado;
+    }
+
+    public double getPrecioXNoche() {
+        return precioXNoche;
+    }
+
+    public void setPrecioXNoche(double precioXNoche) {
+        this.precioXNoche = precioXNoche;
+    }
+
+    public TipoHabitacion getTipo() {
+        return tipo;
+    }
+
+    public int getNumHabitacion() {
+        return numHabitacion;
+    }
+
+    /// Interfaz
 
     @Override
     public int getIdentificador() {
         return this.numHabitacion;
     }
 
+    /// ToString
+
+    @Override
     public String toString()
     {
         return "Habitacion: \n" +
                 "-Numero de Habitacion: " + numHabitacion + "\n" +
                 "-Tipo de Habitacion: " + tipo.toString() + "\n" +
-                "-Estado Actual: " + estado.toString() + "\n";
+                "-Estado Actual: " + estado.toString() + "\n" +
+                "-Precio por Noche: " + precioXNoche + "\n";
     }
 }
